7ca56f4a3d78b5c744affe5694b2f36782e308d9
Mutersec <brixtonhub@protonmail.com>
2023-05-21 17:22:19 +0300

    Dizi elamanlarını işeleme

--- dizi elamanlarını işleme/dizi elemanlarını işleme.lua
+++ dizi elamanlarını işleme/dizi elemanlarını işleme.lua
@@ -1,11 +1,3 @@
-# Lua-Coding
-
-Discord sunucuma beklerim 
-
-Discord:https://discord.gg/WRWEk6jKG7
-
-Bu kod örneği, bir dizinin elemanlarını işleme, dizi toplamını hesaplama, en büyük elemanı bulma, bir fonksiyon tanımlama ve faktoriyel hesaplama gibi gelişmiş özellikleri içermektedir. İleri düzey özelliklerle Lua dilinde daha karmaşık kodlar yazabilirsiniz.
-
 
 local dizi = {10, 20, 30, 40, 50}
 
